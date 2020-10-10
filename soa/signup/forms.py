199f28f9c5c4f15aa497e8918bdf6adffe31cad4
just-a-pancil <sl0nh0ttabuch@gmail.com>
2020-10-10 16:57:53 +0300

    Pipfile deleted

--- soa/signup/forms.py
+++ soa/signup/forms.py
@@ -7,7 +7,7 @@ class CreateUserForm(UserCreationForm):
     email = forms.EmailField(max_length=60, help_text='Нужен для восстановления пароля')
     first_name = forms.CharField(max_length=20,label='Имя')
     last_name = forms.CharField(max_length=20,label='Фамилия')
-    username = forms.CharField(max_length=20,help_text='''Только буквы, цифры и символы @/./+/-/_.''',label='Имя пользователя')
+    username = forms.CharField(max_length=20,help_text='''Ваш логин. Удобно использовать почту''',label='Имя пользователя')
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', "email", "password1", "password2"]
